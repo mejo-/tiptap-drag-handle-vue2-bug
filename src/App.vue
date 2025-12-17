926b175f66e0104b145647fd111305a59ed8b94f
Jonas <jonas@freesources.org>
2025-12-17 14:56:58 +0100

    Initial commit
    Signed-off-by: Jonas <jonas@freesources.org>

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import TiptapEditor from './components/TiptapEditor.vue'
+</script>
+
+<template>
+	<div id="app">
+		<header>
+		<h1>Tiptap DragHandle Vue Bug Demo</h1>
+		</header>
+
+		<main>
+			<TiptapEditor />
+		</main>
+	</div>
+</template>
+
+<style scoped>
+header {
+	line-height: 1.5;
+}
+
+#app {
+	display: flex;
+	flex-direction: column;
+}
+</style>
---
+++ src/components/TiptapEditor.vue
@@ -0,0 +1,76 @@
+<template>
+	<div class="editor-wrapper">
+		<DragHandle v-if="showDragHandle" :editor="editor">
+			<div>⠿</div>
+		</DragHandle>
+		<EditorContent :editor="editor" />
+	</div>
+</template>
+
+<script>
+import { Editor, EditorContent } from '@tiptap/vue-2'
+import StarterKit from '@tiptap/starter-kit'
+import DragHandle from '@tiptap/extension-drag-handle-vue-2'
+
+export default {
+	name: 'TiptapEditor',
+	components: {
+		DragHandle,
+		EditorContent,
+	},
+	data() {
+		return {
+			editor: null,
+			selectedNode: null,
+		}
+	},
+	computed: {
+		showDragHandle() {
+			return this.editor
+		},
+	},
+	mounted() {
+		this.editor = new Editor({
+			extensions: [
+				StarterKit,
+			],
+			content: `
+				<h1>Tiptap with Drag Handle</h1>
+				<p>This is a paragraph</p>
+				<p>Another paragraph</p>
+			`,
+		})
+	},
+	beforeDestroy() {
+		this.editor?.destroy()
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+.editor-wrapper {
+	position: relative;
+	border: 1px solid #ccc;
+	border-radius:	8px;
+	padding:	50px;
+}
+
+/* Basic Tiptap styles */
+.editor-wrapper :deep(.ProseMirror) {
+	outline: none;
+	min-height: 350px;
+
+	h1 {
+		font-size: 2em;
+		margin:	0.67em 0;
+	}
+
+	p {
+		margin: 0.5em 0;
+	}
+
+	ul, ol {
+		padding: 0 1rem;
+	}
+}
+</style>
